53bb0002e5a7b0894545d335d717f9df9c5f6160
Bee <54086109+asdbee@users.noreply.github.com>
2020-10-21 20:17:28 -0400

    Update create&edit-channels.js

--- create&edit-channels.js
+++ create&edit-channels.js
@@ -1,5 +1,5 @@
 let cid = bot.createChannel(msg.guildID,'CHANNEL NAME',0).then(async (cid) => {
-await cid.editPermission('USER ID','117760','0','member','Permission Edit'),
+await cid.editPermission('USER ID','117760','0','member','Permission Edit')
 })
 
 // <channel>.editPermission() can be used in different functions. Editing role permissions.
